34a19c336ef4c3008c612128102b056361d68d52
Sone <sthaya@gmail.com>
2020-08-16 20:52:52 -0700

    created restaurant list component and styled

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,17 @@ import RestaurantDetailPage from './routes/RestaurantDetailPage';
 
 
 const App = () => {
-  return <div>
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/restaurants/:id/update" component={UpdatePage} />
-        <Route exact path="/restaurants/:id" component={RestaurantDetailPage} />
-      </Switch>
-    </Router>
-  </div>
+  return (
+      <div className="container">
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/restaurants/:id/update" component={UpdatePage} />
+            <Route exact path="/restaurants/:id" component={RestaurantDetailPage} />
+          </Switch>
+        </Router>
+      </div>
+  )
 }
 
 export default App
