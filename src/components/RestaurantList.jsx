120d9bfc7b3a4e2c319226a37d203ddc3e2075c7
Sone <sthaya@gmail.com>
2020-08-16 21:15:09 -0700

    created restaurant finder component to retreive data

--- src/components/RestaurantList.jsx
+++ src/components/RestaurantList.jsx
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, { useEffect, useContext } from 'react';
+import RestaurantFinder from '../apis/RestaurantFinder'
+import { RestaurantsContext } from '../context/RestaurantsContext';
 
-const RestaurantList = () => {
+const RestaurantList = (props) => {
+  const { restaurants, setRestaurants } = useContext(RestaurantsContext);
+
+  useEffect(() => { 
+    const fetchData = async () => {
+      try {
+        const response = await RestaurantFinder.get("/");
+        setRestaurants(response.data.data.restaurants);
+     } catch (err) {
+       console.log(err)
+     }
+    }
+
+    fetchData();
+  }, [])
+
   return (
     <div className="list-group">
       <table className="table table-hover table-dark">
