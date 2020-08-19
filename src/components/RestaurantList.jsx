7cc19e2d0c7e27eb4141122c1f63d06396ccfcfc
Sone <sthaya@gmail.com>
2020-08-18 17:59:19 -0700

    completed update page

--- src/components/RestaurantList.jsx
+++ src/components/RestaurantList.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useContext } from 'react';
 import RestaurantFinder from '../apis/RestaurantFinder'
 import { RestaurantsContext } from '../context/RestaurantsContext';
+import { useHistory } from "react-router-dom"
 
 const RestaurantList = (props) => {
   const { restaurants, setRestaurants } = useContext(RestaurantsContext);
+  let history = useHistory()
 
   useEffect(() => { 
     const fetchData = async () => {
@@ -29,6 +31,10 @@ const RestaurantList = (props) => {
     }
   }
 
+  const handleUpdate = (id) => {
+    history.push(`/restaurants/${id}/update`)
+  }
+
   return (
     <div className="list-group">
       <table className="table table-hover table-dark">
@@ -51,7 +57,7 @@ const RestaurantList = (props) => {
                 <td>{"$".repeat(restaurant.price_range)}</td>
                 <td>reviews</td>
                 <td>
-                  <button className="btn btn-warning">Update</button>
+                  <button onClick={() => handleUpdate(restaurant.id)} className="btn btn-warning">Update</button>
                 </td>
                 <td>
                   <button onClick={() => handleDelete(restaurant.id)} className="btn btn-danger">Delete</button>
